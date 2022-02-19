13b060446e5fd4309534692289a69cdf14d0beeb
Néstor Martínez <nestor.marvi@gmail.com>
2022-02-18 21:14:13 -0300

    cambios en el sistema de clonacion.

--- staging.sql
+++ staging.sql
@@ -2,58 +2,13 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.2 (Debian 14.2-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+CREATE TABLE public.users (
+    "ID" integer NOT NULL,
+    "Name" character varying,
+    "Age" integer
+);
 
--- Started on 2022-02-18 21:29:43 UTC
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE postgres;
---
--- TOC entry 3312 (class 1262 OID 13757)
--- Name: postgres; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
-
-
-ALTER DATABASE postgres OWNER TO postgres;
-
-\connect postgres
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 3313 (class 0 OID 0)
--- Dependencies: 3312
--- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
+ALTER TABLE public.users OWNER TO postgres;
 
 CREATE SCHEMA IF NOT EXISTS staging_schema;
 
