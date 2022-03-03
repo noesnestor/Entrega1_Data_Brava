f7dc04519abd5efa61deacecc7078181b3bd8682
Diego Romero <diego.romero@pedidosya.com>
2022-03-03 19:18:00 -0300

    Load staging from heroku

--- airflow/dags/execute_bi_load.py
+++ airflow/dags/execute_bi_load.py
@@ -5,22 +5,30 @@ from airflow import DAG
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-POSTGRES_DEV_CONN_ID = "dev"
+POSTGRES_DEV_CONN_ID = "testing-heroku"
 POSTGRES_STAGING_CONN_ID = "dw"
+
+STAGING_SCHEMA_NAME = "staging"
+TABLES_TO_IMPORT_FROM_DEV = ["polls_empresa", "polls_encuesta", 
+"polls_opcionesrespuesta", "polls_pregunta",
+"polls_respuesta", "polls_rol", "polls_rubro", "polls_tipoencuesta", "polls_tipopregunta", 
+"polls_usuario", "polls_usuario_groups", "polls_usuario_user_permissions"]
 
 # create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
 # instantiating the Postgres Operator
 
-def copy_to_staging(table, temp_file_name):
-    pg_dev_hook = PostgresHook.get_hook(POSTGRES_DEV_CONN_ID)
-    pg_staging_hook = PostgresHook.get_hook(POSTGRES_STAGING_CONN_ID)
+def copy_to_staging(tables, temp_file_name):
+    for table in tables:
+        pg_dev_hook = PostgresHook.get_hook(POSTGRES_DEV_CONN_ID)
+        pg_staging_hook = PostgresHook.get_hook(POSTGRES_STAGING_CONN_ID)
 
-    logging.info("Exporting table: '%s' to file: '%s'", table, temp_file_name)
-    pg_dev_hook.bulk_dump(table, temp_file_name)
+        logging.info("Exporting table: '%s' to file: '%s'", table, temp_file_name)
+        pg_dev_hook.bulk_dump(table, temp_file_name)
 
-    logging.info("Importing table: '%s' from file: '%s'", table, temp_file_name)
-    pg_staging_hook.bulk_load(table, temp_file_name)
+        logging.info("Importing table: '%s' from file: '%s'", table, temp_file_name)
+        pg_staging_hook.bulk_load(STAGING_SCHEMA_NAME+"."+table, temp_file_name)
 
 
 with DAG(
@@ -29,11 +37,17 @@ with DAG(
     schedule_interval="@once",
     catchup=False,
 ) as dag:
+    create_staging_tables = PostgresOperator(
+        task_id="create_staging_tables",
+        postgres_conn_id=POSTGRES_STAGING_CONN_ID,
+        sql="sql/create_staging_tables.sql",
+    )
+
     dev_to_staging = PythonOperator(
         task_id="dev_to_staging",
         python_callable=copy_to_staging,
         op_kwargs={
-            "table": "users",
+            "tables": TABLES_TO_IMPORT_FROM_DEV,
             "temp_file_name": "dump.csv"
         }
     )
@@ -44,7 +58,7 @@ with DAG(
         dag=dag
     )
 
-    dev_to_staging >> staging_to_dw
+    create_staging_tables >> dev_to_staging >> staging_to_dw
     
     
     
---
+++ airflow/dags/sql/create_staging_tables.sql
@@ -0,0 +1,497 @@
+DROP SCHEMA IF EXISTS staging CASCADE; -- TODO: buscar una mejor solucion para hacerlo incremental
+
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE IF NOT EXISTS staging.auth_group (
+    id integer NOT NULL,
+    name character varying(150) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.auth_group_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE staging.auth_group_id_seq OWNED BY staging.auth_group.id;
+
+
+CREATE TABLE IF NOT EXISTS staging.auth_group_permissions (
+    id bigint NOT NULL,
+    group_id integer NOT NULL,
+    permission_id integer NOT NULL
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.auth_group_permissions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE staging.auth_group_permissions_id_seq OWNED BY staging.auth_group_permissions.id;
+
+CREATE TABLE IF NOT EXISTS staging.auth_permission (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    content_type_id integer NOT NULL,
+    codename character varying(100) NOT NULL
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.auth_permission_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE staging.auth_permission_id_seq OWNED BY staging.auth_permission.id;
+
+CREATE TABLE IF NOT EXISTS staging.django_admin_log (
+    id integer NOT NULL,
+    action_time timestamp with time zone NOT NULL,
+    object_id text,
+    object_repr character varying(200) NOT NULL,
+    action_flag smallint NOT NULL,
+    change_message text NOT NULL,
+    content_type_id integer,
+    user_id bigint NOT NULL,
+    CONSTRAINT django_admin_log_action_flag_check CHECK ((action_flag >= 0))
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.django_admin_log_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE staging.django_admin_log_id_seq OWNED BY staging.django_admin_log.id;
+
+CREATE TABLE IF NOT EXISTS staging.django_content_type (
+    id integer NOT NULL,
+    app_label character varying(100) NOT NULL,
+    model character varying(100) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.django_content_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE staging.django_content_type_id_seq OWNED BY staging.django_content_type.id;
+
+CREATE TABLE IF NOT EXISTS staging.django_migrations (
+    id bigint NOT NULL,
+    app character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    applied timestamp with time zone NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.django_migrations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE staging.django_migrations_id_seq OWNED BY staging.django_migrations.id;
+
+
+CREATE TABLE IF NOT EXISTS staging.django_session (
+    session_key character varying(40) NOT NULL,
+    session_data text NOT NULL,
+    expire_date timestamp with time zone NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS staging.polls_empresa (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    nombre_fantasia character varying(30) NOT NULL,
+    rut character varying(20) NOT NULL,
+    certificacion_brava_inicio timestamp with time zone,
+    certificacion_brava_fin timestamp with time zone,
+    cantidad_empleados integer NOT NULL,
+    razon_social character varying(500) NOT NULL,
+    fecha_creacion timestamp with time zone NOT NULL,
+    direccion text NOT NULL,
+    telefono_contacto character varying(30) NOT NULL,
+    mail_contacto character varying(50) NOT NULL,
+    web_empresa character varying(200) NOT NULL,
+    logo character varying(100),
+    cargar_padron character varying(100),
+    ceo_id bigint NOT NULL,
+    rubro_id bigint NOT NULL
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_empresa_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE staging.polls_empresa_id_seq OWNED BY staging.polls_empresa.id;
+
+
+
+CREATE TABLE IF NOT EXISTS staging.polls_encuesta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    esta_completada boolean NOT NULL,
+    fecha_limite timestamp with time zone NOT NULL,
+    titulo character varying(20) NOT NULL,
+    tipo_encuesta_id bigint
+);
+
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_encuesta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+
+ALTER SEQUENCE staging.polls_encuesta_id_seq OWNED BY staging.polls_encuesta.id;
+
+
+
+CREATE TABLE IF NOT EXISTS staging.polls_opcionesrespuesta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    opciones character varying(20) NOT NULL,
+    id_pregunta_id bigint
+);
+
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_opcionesrespuesta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+
+ALTER SEQUENCE staging.polls_opcionesrespuesta_id_seq OWNED BY staging.polls_opcionesrespuesta.id;
+
+
+
+CREATE TABLE IF NOT EXISTS staging.polls_pregunta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    texto character varying(100) NOT NULL,
+    id_encuesta_id bigint,
+    tipo_pregunta_id bigint
+);
+
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_pregunta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+
+ALTER SEQUENCE staging.polls_pregunta_id_seq OWNED BY staging.polls_pregunta.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_respuesta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    respuesta character varying(500) NOT NULL,
+    id_pregunta_id bigint,
+    id_usuario_id bigint
+);
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_respuesta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+ALTER SEQUENCE staging.polls_respuesta_id_seq OWNED BY staging.polls_respuesta.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_rol (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    nombre_rol character varying(20) NOT NULL
+);
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_roles_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+ALTER SEQUENCE staging.polls_roles_id_seq OWNED BY staging.polls_rol.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_rubro (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    rubro character varying(20) NOT NULL
+);
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_rubro_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+ALTER SEQUENCE staging.polls_rubro_id_seq OWNED BY staging.polls_rubro.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_tipoencuesta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    tipo_encuesta character varying(20) NOT NULL
+);
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_tipoencuesta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+
+ALTER SEQUENCE staging.polls_tipoencuesta_id_seq OWNED BY staging.polls_tipoencuesta.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_tipopregunta (
+    id bigint NOT NULL,
+    created timestamp with time zone NOT NULL,
+    removed timestamp with time zone,
+    tipo character varying(20) NOT NULL,
+    dato character varying(20) NOT NULL
+);
+
+
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_tipopregunta_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+ALTER SEQUENCE staging.polls_tipopregunta_id_seq OWNED BY staging.polls_tipopregunta.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_usuario (
+    id bigint NOT NULL,
+    password character varying(128) NOT NULL,
+    last_login timestamp with time zone,
+    is_superuser boolean NOT NULL,
+    username character varying(150) NOT NULL,
+    first_name character varying(150) NOT NULL,
+    last_name character varying(150) NOT NULL,
+    email character varying(254) NOT NULL,
+    is_staff boolean NOT NULL,
+    is_active boolean NOT NULL,
+    date_joined timestamp with time zone NOT NULL,
+    id_empresa_id bigint,
+    id_encuesta_id bigint,
+    rol_id bigint
+);
+
+CREATE TABLE IF NOT EXISTS staging.polls_usuario_groups (
+    id bigint NOT NULL,
+    usuario_id bigint NOT NULL,
+    group_id integer NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_usuario_groups_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE staging.polls_usuario_groups_id_seq OWNED BY staging.polls_usuario_groups.id;
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_usuario_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE staging.polls_usuario_id_seq OWNED BY staging.polls_usuario.id;
+
+CREATE TABLE IF NOT EXISTS staging.polls_usuario_user_permissions (
+    id bigint NOT NULL,
+    usuario_id bigint NOT NULL,
+    permission_id integer NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS staging.polls_usuario_user_permissions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE staging.polls_usuario_user_permissions_id_seq OWNED BY staging.polls_usuario_user_permissions.id;
+
+ALTER TABLE ONLY staging.auth_group ALTER COLUMN id SET DEFAULT nextval('staging.auth_group_id_seq'::regclass);
+ALTER TABLE ONLY staging.auth_group_permissions ALTER COLUMN id SET DEFAULT nextval('staging.auth_group_permissions_id_seq'::regclass);
+ALTER TABLE ONLY staging.auth_permission ALTER COLUMN id SET DEFAULT nextval('staging.auth_permission_id_seq'::regclass);
+ALTER TABLE ONLY staging.django_admin_log ALTER COLUMN id SET DEFAULT nextval('staging.django_admin_log_id_seq'::regclass);
+ALTER TABLE ONLY staging.django_content_type ALTER COLUMN id SET DEFAULT nextval('staging.django_content_type_id_seq'::regclass);
+ALTER TABLE ONLY staging.django_migrations ALTER COLUMN id SET DEFAULT nextval('staging.django_migrations_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_empresa ALTER COLUMN id SET DEFAULT nextval('staging.polls_empresa_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_encuesta ALTER COLUMN id SET DEFAULT nextval('staging.polls_encuesta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_opcionesrespuesta ALTER COLUMN id SET DEFAULT nextval('staging.polls_opcionesrespuesta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_pregunta ALTER COLUMN id SET DEFAULT nextval('staging.polls_pregunta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_respuesta ALTER COLUMN id SET DEFAULT nextval('staging.polls_respuesta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_rol ALTER COLUMN id SET DEFAULT nextval('staging.polls_roles_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_rubro ALTER COLUMN id SET DEFAULT nextval('staging.polls_rubro_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_tipoencuesta ALTER COLUMN id SET DEFAULT nextval('staging.polls_tipoencuesta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_tipopregunta ALTER COLUMN id SET DEFAULT nextval('staging.polls_tipopregunta_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_usuario ALTER COLUMN id SET DEFAULT nextval('staging.polls_usuario_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_usuario_groups ALTER COLUMN id SET DEFAULT nextval('staging.polls_usuario_groups_id_seq'::regclass);
+ALTER TABLE ONLY staging.polls_usuario_user_permissions ALTER COLUMN id SET DEFAULT nextval('staging.polls_usuario_user_permissions_id_seq'::regclass);
+
+ALTER TABLE ONLY staging.auth_group
+    ADD CONSTRAINT auth_group_name_key UNIQUE (name);
+
+ALTER TABLE ONLY staging.auth_group_permissions
+    ADD CONSTRAINT auth_group_permissions_group_id_permission_id_0cd325b0_uniq UNIQUE (group_id, permission_id);
+
+ALTER TABLE ONLY staging.auth_group_permissions
+    ADD CONSTRAINT auth_group_permissions_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.auth_group
+    ADD CONSTRAINT auth_group_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.auth_permission
+    ADD CONSTRAINT auth_permission_content_type_id_codename_01ab375a_uniq UNIQUE (content_type_id, codename);
+
+ALTER TABLE ONLY staging.auth_permission
+    ADD CONSTRAINT auth_permission_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.django_admin_log
+    ADD CONSTRAINT django_admin_log_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.django_content_type
+    ADD CONSTRAINT django_content_type_app_label_model_76bd3d3b_uniq UNIQUE (app_label, model);
+
+ALTER TABLE ONLY staging.django_content_type
+    ADD CONSTRAINT django_content_type_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.django_migrations
+    ADD CONSTRAINT django_migrations_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.django_session
+    ADD CONSTRAINT django_session_pkey PRIMARY KEY (session_key);
+
+ALTER TABLE ONLY staging.polls_empresa
+    ADD CONSTRAINT polls_empresa_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_encuesta
+    ADD CONSTRAINT polls_encuesta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_opcionesrespuesta
+    ADD CONSTRAINT polls_opcionesrespuesta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_pregunta
+    ADD CONSTRAINT polls_pregunta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_respuesta
+    ADD CONSTRAINT polls_respuesta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_rol
+    ADD CONSTRAINT polls_roles_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_rubro
+    ADD CONSTRAINT polls_rubro_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_tipoencuesta
+    ADD CONSTRAINT polls_tipoencuesta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_tipopregunta
+    ADD CONSTRAINT polls_tipopregunta_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_usuario_groups
+    ADD CONSTRAINT polls_usuario_groups_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_usuario_groups
+    ADD CONSTRAINT polls_usuario_groups_usuario_id_group_id_b3e82601_uniq UNIQUE (usuario_id, group_id);
+
+ALTER TABLE ONLY staging.polls_usuario
+    ADD CONSTRAINT polls_usuario_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_usuario_user_permissions
+    ADD CONSTRAINT polls_usuario_user_permi_usuario_id_permission_id_c516b116_uniq UNIQUE (usuario_id, permission_id);
+
+ALTER TABLE ONLY staging.polls_usuario_user_permissions
+    ADD CONSTRAINT polls_usuario_user_permissions_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY staging.polls_usuario
+    ADD CONSTRAINT polls_usuario_username_key UNIQUE (username);
